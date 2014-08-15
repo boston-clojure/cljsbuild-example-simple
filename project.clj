ecae0286211e44de326abee8dc196c77bf6f45cb
Paul Lam <paul@quantisan.com>
2014-08-14 21:05:46 -0400

    init

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject cljsbuild-example-simple "1.0.3"
+  :description "A simple example of how to use lein-cljsbuild"
+  :source-paths ["src-clj"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2311"
+                  :exclusions [org.apache.ant/ant]]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.7"]]
+  :cljsbuild {
+    :builds [{:source-paths ["src-cljs"]
+              :compiler {:output-to "resources/public/js/main.js"
+                         :optimizations :simple
+                         :output-wrapper false
+                         :pretty-print false}}]}
+  :ring {:handler example.routes/app})
---
+++ src-clj/example/views.clj
@@ -0,0 +1,14 @@
+(ns example.views
+  (:require
+    [hiccup
+      [page :refer [html5]]
+      [page :refer [include-js]]]))
+
+(defn index-page []
+  (html5
+    [:head
+     [:title "Hello World"]
+     ;(include-js "/js/out/goog/base.js")
+     (include-js "/js/main.js")]
+    [:body
+     [:h1 "Hello World"]]))
